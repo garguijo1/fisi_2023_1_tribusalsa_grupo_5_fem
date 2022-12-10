ed3bdaf4024679b3f4704e7e1bfef1e860fb1f00
Valerie Arca <ValeJazmin>
2022-12-09 21:41:08 -0500

    Inicio repor Valerie

---
+++ app/src/main/java/com/example/vistas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vistas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fr_reserva_carta_adicional)
+    }
+}
